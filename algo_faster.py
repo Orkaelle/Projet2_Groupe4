42472ae80d465d0038f0c013f5ad568f35d05d16
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-07-28 16:18:33 +0200

    PEP8

--- algo_faster.py
+++ algo_faster.py
@@ -1,45 +1,52 @@
-import cx_Oracle
-import requests
-import datetime
-import csv
+"""
+This algorithm identifies the fastest trip.
+"""
 
-token = ["32309a06-a2c3-4724-9f37-f4d94586b6c7","660ae566-43e0-4eb4-9950-aaed5e567866","078f97ac-1d15-411d-963b-c6e46689e561","9d8450f6-354d-4c82-ba48-771ebb775d7d","35c22f68-836c-4170-80c2-ad26ffe8527f","72078fc4-5d1e-40f0-a5b0-6d29d8bb4af1","dd7fc13b-20c4-43b7-bee6-1995b3d77a60","2e205281-070a-4755-a703-52fa77a9c943","9e78ab8f-aa47-4261-a036-566f67929ada","9bf360f1-dcb2-40f2-acc3-dee3f60c8f20","91a3c8b6-10a5-454b-8191-4e391dd3ec9f"]
-index_token = 3
+import datetime
+
+import csv
+import requests
+
+import cx_Oracle
+
+
+token = [
+    "32309a06-a2c3-4724-9f37-f4d94586b6c7",
+    "660ae566-43e0-4eb4-9950-aaed5e567866",
+    "078f97ac-1d15-411d-963b-c6e46689e561",
+    "9d8450f6-354d-4c82-ba48-771ebb775d7d",
+    "35c22f68-836c-4170-80c2-ad26ffe8527f",
+    "72078fc4-5d1e-40f0-a5b0-6d29d8bb4af1"]
+index_token = 0
 tokenAPI = token[index_token]
 
 
 # DataBase Connexion
 conn = cx_Oracle.connect("admin", "Simplon.co63", "simplon_high")
 print(conn.version)
-cur=conn.cursor()
+cur = conn.cursor()
 
 # Extracting prefecture codes
 cur.execute('SELECT city_id_pref FROM departementmetro')
 result = cur.fetchall()
 codes_pref = []
-for r in result :
+for r in result:
     codes_pref.append(r[0])
 
 
 # Find start Code :
-index_start = codes_pref.index(5061) #Gap
+index_start = codes_pref.index(5061)  # Gap
 
 # Priorities
 priorities = ['2408', '5061', '26198', '23096', '60057', '4112']
 priority_trips = []
 
-
-'''
-# test version
-codes_pref = [6088, 10387, 15014, 59350]
-'''
-
 # start point
 start_from = codes_pref[index_start]
 start_time = "20200803T060000"
 
-print ('\n')
-print ('Start from ' + str(start_from))
+print('\n')
+print('Start from ' + str(start_from))
 codes_pref.remove(start_from)
 
 faster_route = []
@@ -48,35 +55,40 @@ temp = []
 
 
 # first trip
-for c in codes_pref :
+for c in codes_pref:
     depart = str(start_from)
     destination = str(c)
     depart_datetime = start_time
 
-    print ('to ' + destination)
+    print('to ' + destination)
 
     journey_list = []
 
-    lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf/journeys?from=admin%3Afr%3A" + depart + "&to=admin%3Afr%3A" + destination + "&datetime=" + depart_datetime + "&max_nb_transfers=2&count=3&"
+    lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf/journeys?from=admin%3Afr%3A" + depart + \
+        "&to=admin%3Afr%3A" + destination + "&datetime=" + depart_datetime + "&max_nb_transfers=2&count=3&"
     data = requests.get(lien).json()
 
-    try :
+    try:
         message = data['message']
         print(message)
-        print ('Retry with another token...')
-        tokenAPI = token[index_token+1]
-        lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf/journeys?from=admin%3Afr%3A" + depart + "&to=admin%3Afr%3A" + destination + "&datetime=" + depart_datetime + "&max_nb_transfers=2&count=3&"
+        print('Retry with another token...')
+        tokenAPI = token[index_token + 1]
+        lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf/journeys?from=admin%3Afr%3A" + depart + \
+            "&to=admin%3Afr%3A" + destination + "&datetime=" + depart_datetime + "&max_nb_transfers=2&count=3&"
         data = requests.get(lien).json()
-    except :
+    except BaseException:
         nada = []
-    
-    try :
-        journeys = data['journeys']
-        print (str(len(journeys)) + " trips found.")
 
-        for j in journeys :
+    try:
+        journeys = data['journeys']
+        print(str(len(journeys)) + " trips found.")
+
+        for j in journeys:
             arrival_datetime = j["arrival_date_time"]
-            temp.append(datetime.datetime.strptime(arrival_datetime,'%Y%m%dT%H%M%S%f'))
+            temp.append(
+                datetime.datetime.strptime(
+                    arrival_datetime,
+                    '%Y%m%dT%H%M%S%f'))
 
         index_faster = temp.index(min(temp))
         temp.clear()
@@ -85,93 +97,104 @@ for c in codes_pref :
         co2 = journeys[index_faster]["co2_emission"]["value"]
         departure_time = journeys[index_faster]["departure_date_time"]
         arrival_time = journeys[index_faster]["arrival_date_time"]
-        depart_station = journeys[index_faster]["sections"][0]["to"]['stop_point']['stop_area']['id'].replace(':','%3A')
-        max_sections = len(journeys[index_faster]["sections"])-1
-        arrival_station = journeys[index_faster]["sections"][max_sections]['from']['stop_point']['stop_area']['id'].replace(':','%3A')
+        depart_station = journeys[index_faster]["sections"][0]["to"]['stop_point']['stop_area']['id'].replace(
+            ':', '%3A')
+        max_sections = len(journeys[index_faster]["sections"]) - 1
+        arrival_station = journeys[index_faster]["sections"][max_sections]['from']['stop_point']['stop_area']['id'].replace(
+            ':', '%3A')
 
-        faster_trip.append([depart, depart_station, destination, arrival_station, departure_time, duration, arrival_time, co2, "F"])
+        faster_trip.append([depart,
+                            depart_station,
+                            destination,
+                            arrival_station,
+                            departure_time,
+                            duration,
+                            arrival_time,
+                            co2,
+                            "F"])
+        print("Faster trip saved.")
 
-        print ("Faster trip saved.")
-
-    except :
-        try :
+    except BaseException:
+        try:
             error = requests.get(lien).json()['error']['message']
-            print (error)
-        except :
-            print ('ERROR')
-    
+            print(error)
+        except BaseException:
+            print('ERROR')
+
 temp.clear()
 priority_trips.clear()
 
 
-for f in faster_trip :
-    if f[2] in priorities :
+for f in faster_trip:
+    if f[2] in priorities:
         priority_trips.append([faster_trip.index(f), f[6]])
-    else :
+    else:
         temp.append(f[6])
 
-if priority_trips != [] :
+if priority_trips != []:
     temp.clear()
-    for p in priority_trips :
+    for p in priority_trips:
         temp.append(p[1])
     index_priority = temp.index(min(temp))
     index_faster = priority_trips[index_priority][0]
-else :
+else:
     index_faster = temp.index(min(temp))
 
 faster_route.append(faster_trip[index_faster])
 
-print ("Starter faster route :")
-print (faster_route)
-
+print("Starter faster route :")
+print(faster_route)
 
 
 # Next trips
-
-while codes_pref != [] :
+while codes_pref != []:
 
     depart_pref = faster_route[-1][2]
     depart_station = faster_route[-1][3]
     depart_time = faster_route[-1][6]
 
-    print ('\n')
-    print ('Next step from ' + str(depart_pref))
+    print('\n')
+    print('Next step from ' + str(depart_pref))
     codes_pref.remove(int(depart_pref))
 
     faster_trip.clear()
     temp.clear()
 
-    for c in codes_pref :
+    for c in codes_pref:
         depart = str(depart_station)
         destination = str(c)
         depart_datetime = str(depart_time)
 
-        print ('to ' + destination)
+        print('to ' + destination)
 
         journey_list = []
 
-        lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf//journeys?from=" + depart + "&to=admin%3Afr%3A" + destination + "&max_nb_transfers=2&count=3&datetime=" + depart_datetime + "&datetime_represents=departure&"
+        lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf//journeys?from=" + depart + "&to=admin%3Afr%3A" + \
+            destination + "&max_nb_transfers=2&count=3&datetime=" + depart_datetime + "&datetime_represents=departure&"
         data = requests.get(lien).json()
 
-        try :
+        try:
             message = data['message']
             print(message)
-            print ('Retry with another token...')
-            tokenAPI = token[index_token+1]
-            lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf//journeys?from=" + depart + "&to=admin%3Afr%3A" + destination + "&max_nb_transfers=2&count=3&datetime=" + depart_datetime + "&datetime_represents=departure&"
+            print('Retry with another token...')
+            tokenAPI = token[index_token + 1]
+            lien = "https://" + tokenAPI + ":@api.navitia.io/v1/coverage/sncf//journeys?from=" + depart + "&to=admin%3Afr%3A" + \
+                destination + "&max_nb_transfers=2&count=3&datetime=" + depart_datetime + "&datetime_represents=departure&"
             data = requests.get(lien).json()
 
-
-        except :
+        except BaseException:
             nada = []
 
-        try :
+        try:
             journeys = data['journeys']
-            print (str(len(journeys)) + " trips found.")
+            print(str(len(journeys)) + " trips found.")
 
-            for j in journeys :
+            for j in journeys:
                 arrival_datetime = j["arrival_date_time"]
-                temp.append(datetime.datetime.strptime(arrival_datetime,'%Y%m%dT%H%M%S%f'))
+                temp.append(
+                    datetime.datetime.strptime(
+                        arrival_datetime,
+                        '%Y%m%dT%H%M%S%f'))
 
             index_faster = temp.index(min(temp))
             temp.clear()
@@ -180,60 +203,68 @@ while codes_pref != [] :
             co2 = journeys[index_faster]["co2_emission"]["value"]
             departure_time = journeys[index_faster]["departure_date_time"]
             arrival_time = journeys[index_faster]["arrival_date_time"]
-            depart_station = journeys[index_faster]["sections"][0]["to"]['stop_point']['stop_area']['id'].replace(':','%3A')
-            max_sections = len(journeys[index_faster]["sections"])-1
-            arrival_station = journeys[index_faster]["sections"][max_sections]['from']['stop_point']['stop_area']['id'].replace(':','%3A')
+            depart_station = journeys[index_faster]["sections"][0]["to"]['stop_point']['stop_area']['id'].replace(
+                ':', '%3A')
+            max_sections = len(journeys[index_faster]["sections"]) - 1
+            arrival_station = journeys[index_faster]["sections"][max_sections]['from']['stop_point']['stop_area']['id'].replace(
+                ':', '%3A')
 
-            faster_trip.append([depart_pref, depart_station, destination, arrival_station, departure_time, duration, arrival_time, co2, "F"])
+            faster_trip.append([depart_pref,
+                                depart_station,
+                                destination,
+                                arrival_station,
+                                departure_time,
+                                duration,
+                                arrival_time,
+                                co2,
+                                "F"])
 
-            print ("Faster trip saved.")
+            print("Faster trip saved.")
 
-        except :
-            try :
+        except BaseException:
+            try:
                 message = requests.get(lien).json()['message']
                 print(message)
-            except :
-                try :
+            except BaseException:
+                try:
                     error = requests.get(lien).json()['error']['message']
-                    print (error)
-                except :
-                    print ('ERROR')
-        
+                    print(error)
+                except BaseException:
+                    print('ERROR')
+
     temp.clear()
     priority_trips.clear()
 
-    if faster_trip :
-        for f in faster_trip :
-            if f[2] in priorities :
+    if faster_trip:
+        for f in faster_trip:
+            if f[2] in priorities:
                 priority_trips.append([faster_trip.index(f), f[6]])
             temp.append(f[6])
-        
-        if priority_trips :
-            print (priority_trips)
+
+        if priority_trips:
+            print(priority_trips)
             temp.clear()
-            for p in priority_trips :
+            for p in priority_trips:
                 temp.append(p[1])
             index_priority = temp.index(min(temp))
             index_faster = priority_trips[index_priority][0]
-        else :
+        else:
             index_faster = temp.index(min(temp))
-
 
         faster_route.append(faster_trip[index_faster])
 
-        print ("Next faster route :")
-        print (faster_route[-1])
+        print("Next faster route :")
+        print(faster_route[-1])
 
-        with open('faster.csv', 'w', newline='') as f :
+        with open('faster.csv', 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(faster_route)
-    
-    else :
-        print ('No trip found for this route !')
-        print ('\n')
+
+    else:
+        print('No trip found for this route !')
+        print('\n')
 
 
-
-print ('\n\n')
-print ('Faster route :')
-print (faster_route)
+print('\n\n')
+print('Faster route :')
+print(faster_route)
--- stat.py
+++ stat.py
@@ -1,34 +1,38 @@
+'''
+This file is used to calculate the global time of the fastest journey,
+and the total CO2 emissions of the grennest one.
+'''
+
 import csv
 import datetime
 
 co2 = []
 
-print ('\n')
+print('\n')
 
-with open ('faster.csv', newline = '') as f :
+with open('faster.csv', newline='') as f:
     reader = list(csv.reader(f))
 
     depart_time = reader[0][-3]
     arrival_time = reader[-1][-3]
 
-    start = datetime.datetime.strptime(depart_time,'%Y%m%dT%H%M%S%f')
-    end = datetime.datetime.strptime(arrival_time,'%Y%m%dT%H%M%S%f')
+    start = datetime.datetime.strptime(depart_time, '%Y%m%dT%H%M%S%f')
+    end = datetime.datetime.strptime(arrival_time, '%Y%m%dT%H%M%S%f')
 
     total_time = end - start
-    print ('Total time :')
-    print (total_time)
+    print('Total time :')
+    print(total_time)
 
-print ('\n')
+print('\n')
 
-with open ('greener.csv', newline='') as g :
+with open('greener.csv', newline='') as g:
     reader = csv.reader(g)
 
-    for row in reader :
+    for row in reader:
         co2.append(float(row[-2]))
 
-    total_co2 = round(sum(co2),4)
-    print ('Total CO2 (gEC) :')
-    print (total_co2)
+    total_co2 = round(sum(co2), 4)
+    print('Total CO2 (gEC) :')
+    print(total_co2)
 
-print ('\n')
-        
+print('\n')
